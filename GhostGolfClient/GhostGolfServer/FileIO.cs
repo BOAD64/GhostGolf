44dcbfb289490ecc5d0c303fc62bbe511430ad8f
JochemDG <Pipsquek542>
2021-10-05 10:21:15 +0200

    Modified updatePar method

--- GhostGolfClient/GhostGolfServer/FileIO.cs
+++ GhostGolfClient/GhostGolfServer/FileIO.cs
@@ -5,7 +5,7 @@ namespace GhostGolfServer
 {
     public class FileIO
     {
-        private static string filePath = $"{System.Environment.CurrentDirectory}/GhostGolf.txt";
+        private static readonly string filePath = $"{Environment.CurrentDirectory}/GhostGolf.txt";
         private ServerUtil util;
 
         public FileIO()
@@ -24,11 +24,15 @@ namespace GhostGolfServer
         public void UpdatePar(int score)
         {
             string[] content = File.ReadAllLines(filePath);
-            double par = Double.Parse(content[0].Substring(0, content[0].IndexOf(" ")));
-            int weight = Int32.Parse(content[0].Substring(content[0].LastIndexOf(" ")));
 
-            Tuple<double, int> newPar = this.util.calculatePar(par, weight, score);
-            content[0] = $"{newPar.Item1} - {newPar.Item2}";
+            try {
+                Tuple<double, int> newPar = this.util.calculatePar(Double.Parse(content[0].Substring(0, content[0].IndexOf(" "))),
+                    Int32.Parse(content[0].Substring(content[0].LastIndexOf(" "))), score);
+                content[0] = $"{newPar.Item1} - {newPar.Item2}";
+            } catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             File.WriteAllLines(filePath, content);
         }
@@ -51,8 +55,16 @@ namespace GhostGolfServer
         public void AddHighscore(string userName, int score)
         {
             StreamWriter writer = File.AppendText(filePath);
-            writer.WriteLine($"{userName} {s}");
+            writer.WriteLine($"{userName} {score}");
             writer.Close();
+        }
+
+        public int GetPar()
+        {
+            string[] content = File.ReadAllLines(filePath);
+            double par = Double.Parse(content[0].Substring(0, content[0].IndexOf(" ")));
+
+            return (int)par;
         }
     }
 }
